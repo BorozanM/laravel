ec405a53271eebe5703dbbb18629324f4082f53a
marijaborozan <borozankaa@gmail.com>
2023-05-24 23:09:15 +0200

    seedovana baza

--- laptopapp/app/Models/Racun.php
+++ laptopapp/app/Models/Racun.php
@@ -6,13 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Racun extends Model
-{
-    use HasFactory;protected $fillable = [
+{public $timestamps = false;
+    use HasFactory;
+
+    protected $fillable = [
         'datum',
-        'adresa',
+        'adresaLokala',
+
+
     ];
     public function stavke()
     {
         return $this->hasMany(StavkaRacuna::class);
     }
+
 }
